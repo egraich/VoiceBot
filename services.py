95e63fa2b3ca644380ab29c2e19f0956aa434125
egraich <egorbabchuk1@gmail.com>
2026-07-17 22:04:26 +0300

    fix: audio format

---
+++ services.py
@@ -0,0 +1,63 @@
+import os
+import asyncio
+import logging
+from groq import AsyncGroq
+import config
+
+groq_client = AsyncGroq(api_key=config.GROQ_API_KEY)
+logger = logging.getLogger(__name__)
+
+async def extract_audio(input_path: str, output_path: str) -> bool:
+    """
+    Extract and convert sound from video files.
+    """
+    logger.info(f"Запуск FFmpeg: извлечение аудио из {input_path}")
+    
+    try:
+        process = await asyncio.create_subprocess_exec(
+            'ffmpeg', '-y', '-i', input_path,
+            '-vn',                
+            '-map', '0:a',        
+            '-ar', '16000',       
+            '-ac', '1',           
+            '-c:a', 'flac',       
+            output_path,          
+            stdout=asyncio.subprocess.DEVNULL,
+            stderr=asyncio.subprocess.DEVNULL
+        )
+        
+        await process.communicate()
+        
+        if process.returncode == 0 and os.path.exists(output_path):
+            logger.info(f"FFmpeg успешно завершил работу: {output_path}")
+            return True
+        else:
+            logger.error(f"Ошибка FFmpeg, код возврата: {process.returncode}")
+            return False
+            
+    except Exception as e:
+        logger.error(f"Исключение при вызове FFmpeg: {e}")
+        return False
+
+async def transcribe_audio(audio_path: str) -> str | None:
+    """
+    Send audio to Groq API and return answer.
+    """
+    logger.info(f"Отправка файла в Groq API: {audio_path}")
+    
+    try:
+        with open(audio_path, "rb") as file:
+            transcription = await groq_client.audio.transcriptions.create(
+                file=(os.path.basename(audio_path), file.read()),
+                model=config.GROQ_MODEL,
+                response_format="json",
+                temperature=0.0
+            )
+        
+        result_text = transcription.text.strip() if transcription.text else None
+        logger.info(f"Groq успешно вернул текст длиной {len(result_text) if result_text else 0} симв.")
+        return result_text
+        
+    except Exception as e:
+        logger.error(f"Ошибка при работе с Groq API: {e}")
+        return None
